eb0c044e2e70517f611d90663d63c815214a660b
Devin Moore <devin.moore@volken.org>
2017-12-26 22:37:24 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,2 @@
 def display_board
-  a_tic_tac_toe_board# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+  a_tic_tac_toe_board = # Define a method display_board that prints a 3x3 Tic Tac Toe Board
